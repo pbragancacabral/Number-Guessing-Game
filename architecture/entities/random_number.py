c60c4a0f67ce838d5d0f62132dfe230a3bef37d8
Pedro de Bragança Cabral <pbragancacabral@gmail.com>
2019-01-30 10:15:03 +0100

    Reset project back to initial files
    I believe I was over-engineering the project, without using OOP I find
    it difficult to do all the necessary separation of concerns between
    the architecture and the delivery mechanism. I will approach it in a
    more procedural fashion and leave these architectural decisions for a
    later stage or a future project revisit.

--- architecture/entities/random_number.py
+++
@@ -1,5 +0,0 @@
-import random
-
-
-def generate_random_number():
-    return random.randint(1, 2)
--- delivery/view/terminal.py
+++
@@ -1,6 +0,0 @@
-def ask_guess():
-    return input("Guess?")
-
-
-def display(message):
-    print(message)
