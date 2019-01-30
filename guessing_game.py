299f85fc1f5ca84e9f955479f966fb96b83e66a0
Pedro de Bragança Cabral <pbragancacabral@gmail.com>
2019-01-30 12:19:51 +0100

    Fix minor bugs

--- guessing_game.py
+++ guessing_game.py
@@ -1,44 +1,91 @@
+import os
 import random
-import sys
 
-answer = random.randint(1, 2)
-print("DEBUG:", answer)
 high_score = None
 
 
+def display_error(error):
+    print(f"Error: {error}")
+
+
 def display_header():
-    print("Welcome")
+    print("""===================================
+Welcome to the Number Guessing Game
+===================================""")
 
 
-def display_menu():
-    print("play? ")
-    print("quit? ")
-    return input("what?")
+def ask_option():
+    print("PLAY - to play the game")
+    print("QUIT - to quit the game ")
+    print()
+    return input("Please select an option: ")
 
 
-def play():
+def ask_for_guess():
+    guess = int(input("Please select a number between 1 and 10: "))
+    if guess < 1 or guess > 10:
+        raise ValueError("That number is out of range.")
+    return guess
+
+
+def give_hint(user_guess, answer):
+    if user_guess != answer:
+        if user_guess < answer:
+            print("Try higher.")
+        else:
+            print("Try lower.")
+
+
+def guess():
     global high_score
+    print("High score (least guesses):", "not set" if high_score is None else high_score)
+    answer = random.randint(1, 10)
+    user_guess = None
     number_of_attempts = 0
-    guess = int(input("Guess?"))
-    number_of_attempts += 1
-    while guess != answer:
-        guess = int(input("Guess?"))
-        number_of_attempts += 1
+
+    while user_guess != answer:
+        try:
+            user_guess = ask_for_guess()
+            number_of_attempts += 1
+            give_hint(user_guess, answer)
+        except ValueError as error:
+            display_error(error)
+            continue
 
     print("Took you:", number_of_attempts, "attempts")
 
     if not high_score or high_score > number_of_attempts:
         high_score = number_of_attempts
-        print("BEATED!")
+        print("You have set a new record!")
 
-    again = input("Play again?")
-    if again == "yes":
-        play()
+    again = input("Play again? YES/NO: ")
+    clear_screen()
+    if again.upper() == "YES":
+        guess()
 
 
-display_header()
-choice = display_menu()
-if choice == "quit":
-    sys.exit()
-elif choice == "play":
-    play()
+def clear_screen():
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def display_menu():
+    while True:
+        try:
+            option = ask_option().upper()
+            if option == "QUIT":
+                clear_screen()
+                break
+            elif option == "PLAY":
+                clear_screen()
+                guess()
+            else:
+                raise ValueError("That's not an option.")
+        except ValueError as error:
+            display_error(error)
+        else:
+            break
+
+
+if __name__ == "__main__":
+    display_header()
+    display_menu()
